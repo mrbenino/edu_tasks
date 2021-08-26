8c62f926a1fe0ba6eb364797da87df53d58e827a
rlaktionov <rlaktionov@trading-point.com>
2021-08-26 10:33:28 +0300

    - preparations;

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
-import begin.Begin;
 import begin.BeginKt;
 import core.Command;
 
 public class Main {
 
     public static void main(String[] args) {
-        Command beginKt = new BeginKt(); // BeginKt -> Command
+        Command beginKt = new BeginKt();
         beginKt.execute();
     }
 }
---
+++ src/integer/IntegerKt.kt
@@ -0,0 +1,10 @@
+package integer
+
+import core.Command
+
+class IntegerKt : Command {
+
+    override fun execute() {
+        TODO("Not yet implemented")
+    }
+}
