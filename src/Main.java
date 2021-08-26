204e246bd8b7558d7afe3214eff4b3916f3032f7
mrbenino <beninbenino@gmail.com>
2021-08-26 09:31:25 +0300

    Перенес вызов методов kotlin в команду

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,6 @@ public class Main {
     public static void main(String[] args) {
         Command beginKt = new BeginKt(); // BeginKt -> Command
         beginKt.execute();
-
-        //TODO: add same realization for Java variant
-
-        // TODO: move realization of each task to appropriate method
-
     }
 }
 
--- src/begin/Begin.kt
+++ src/begin/Begin.kt
@@ -9,34 +9,73 @@ import kotlin.math.sqrt
 class BeginKt : Command {
 
     override fun execute() {
-        println("Обьем прямоугольного параллелепипеда: " + volume(10, 12, 15))
-        println("Площадь прямоугольного параллелепипеда: " + square(10, 12, 15))
-        println("Длина окружности: " + length(10))
-        println("Площадь окружности: " + square(10.0))
-        println("Среднее арифметическое: " + average(44, 45))
-        println("Среднее геометрическое: " + geometricMean(144, 674))
-        println("Сумма модулей: " + sum(144.0, 674.0))
-        println("Разность модулей: " + subtraction(144.0, 674.0))
-        println("Произведение модулей: " + multiplication(144.0, 674.0))
-        println("Частное модулей: " + division(144.0, 674.0))
-        println("Сумма квадратов: " + sumSqrt(144.0, 674.0))
-        println("Разность квадратов: " + subtractionSqrt(144.0, 674.0))
-        println("Произведение квадратов: " + multiplicationSqrt(144.0, 674.0))
-        println("Частное квадратов: " + divisionSqrt(144.0, 674.0))
-        println("Гипотинуза триугольника: " + hypotenuse(144.0, 674.0))
-        println("Перимитр триугольника: " + perimeter(144.0, 674.0))
-        println("Площадь кольца: " + squareRing(44.0, 6.0))
-        println("Радиус из длины окружности: " + radiusOfLength(40.0))
-        println("Площадь из длины окружности: " + squareOfLength(44.0))
-        println("Диаметр окружности из площади: " + diameterOfSquare(100.0))
-        println("Длина окружности из площади: " + lengthOfSquare(100.0))
-        println("Растояние между двумя точками: " + distanceBetween(10.0, 12.0))
+        println(begin5())
+        println(begin7())
+        println(begin8())
+        println(begin9())
+        println(begin10())
+        println(begin11())
+        println(begin12())
+        println(begin13())
+        println(begin14())
+        println(begin15())
+        println(begin16())
     }
 
     fun begin5() {
         val (volume, squareCube) = volume(11.0)
         System.out.printf("Обьем куба: %f, площадь куба: %f%n", volume, squareCube)
     }
+
+    fun begin7() {
+        val (square, length) = square(10.0)
+        System.out.printf("Длина окружности: %f,  площадь круга: %f%n", square, length)
+    }
+
+    fun begin8() {
+        val average = average(10.0, 24.0)
+        System.out.printf("Cреднее арифметическое: %f %n", average)
+    }
+
+    fun begin9() {
+        val geometricMean = geometricMean(22.0, 44.0)
+        System.out.printf("Cреднее геометрическое: %f  %n", geometricMean)
+    }
+
+    fun begin10() {
+        val sum = sum(10.0, 20.0)
+        System.out.println(sum)
+    }
+
+    fun begin11() {
+        val mySqlt = mySqlt(10.0, 20.0)
+        System.out.println(mySqlt)
+    }
+
+    fun begin12() {
+        val hypotenuse = hypotenuse(10.0, 20.0)
+        System.out.println(hypotenuse)
+    }
+
+    fun begin13() {
+        System.out.println(squareRing(20.0, 10.0))
+    }
+
+    fun begin14() {
+        val (radius, square) = squareOfLength(12.2)
+        System.out.printf("Радиус круга: %f площядь круга: %f%n", radius, square)
+    }
+
+    fun begin15() {
+        val (diameter, length) = diameterOfSquare(10.0)
+        System.out.printf("Диаметр : %f, длина: %f%n", diameter, length)
+    }
+
+    fun begin16()  {
+        val distanceBetween = distanceBetween(20.0,40.0)
+        System.out.printf("Расстояние между двумя точками: " + distanceBetween)
+    }
+
 
     /**
      * Дана длина ребра куба a. Найти объем куба V = a3 и площадь его по-
@@ -49,155 +88,72 @@ class BeginKt : Command {
     }
 
     /**
-     * Даны длины ребер a, b, c прямоугольного параллелепипеда. Найти его
-     * объем V = a·b·c и площадь поверхности S = 2·(a·b + b·c + a·c).
-     */
-    private fun volume(a: Int, b: Int, c: Int): Int {
-        return a * b * c
-    }
-
-    /**
-     * Даны длины ребер a, b, c прямоугольного параллелепипеда. Найти его
-     * объем V = a·b·c и площадь поверхности S = 2·(a·b + b·c + a·c).
-     */
-    private fun square(a: Int, b: Int, c: Int): Int {
-        var result = a * b
-        result += b * c
-        result += a * c
-        return result * 2
-    }
-
-    /**
      * Найти длину окружности L и площадь круга S заданного радиуса R: L = 2·π·R, S = π·R2.
      * В качестве значения π использовать 3.14.
      */
-    private fun square(r: Double): Double {
-        return Math.PI * (r * r)
-    }
-
-    /**
-     * Найти длину окружности L и площадь круга S заданного радиуса R: L = 2·π·R, S = π·R2.
-     * В качестве значения π использовать 3.14.
-     */
-    private fun length(r: Int): Double {
-        return Math.PI * (2 * r)
+    private fun square(r: Double): Pair<Double, Double> {
+        val square = Math.PI * (r * r)
+        val length = Math.PI * (2 * r)
+        return square to length
     }
 
     /**
      * Даны два числа a и b. Найти их среднее арифметическое: (a + b)/2
      */
-    private fun average(a: Int, b: Int): Int {
+    private fun average(a: Double, b: Double): Double {
         return (a + b) / 2
     }
 
-    private fun geometricMean(a: Int, b: Int): Double {
-        //TODO: add reason
+    /**
+     * Даны два неотрицательных числа a и b. Найти их среднее геометриче-
+     * ское, то есть квадратный корень из их произведения:
+     */
+    private fun geometricMean(a: Double, b: Double): Double {
+
         if (a < 0 || b < 0) error("placeholder")
 
-        val result = a * b
-        return sqrt(result.toDouble())
+        return sqrt(a * b)
     }
 
     /**
      * Даны два ненулевых числа. Найти сумму, разность, произведение и частное их модулей.
      */
-    private fun sum(a: Double, b: Double): Double {
-        return if (a == 0.0 || b == 0.0) {
-            0.0
-        } else abs(a) + abs(b)
-    };
-
-    /**
-     * Даны два ненулевых числа. Найти сумму, разность, произведение и частное их модулей.
-     */
-    private fun subtraction(a: Double, b: Double): Double {
-        return if (a == 0.0 || b == 0.0) {
-            0.0
-        } else abs(a) - abs(b)
-    };
-
-    /**
-     * Даны два ненулевых числа. Найти сумму, разность, произведение и частное их модулей.
-     */
-    private fun multiplication(a: Double, b: Double): Double =
+    private fun sum(a: Double, b: Double): String {
         if (a == 0.0 || b == 0.0) {
-            0.0
+            error("placeholder")
         } else {
-            abs(a) * abs(b)
-        }
-
-
-    /**
-     * Даны два ненулевых числа. Найти сумму, разность, произведение и частное их модулей.
-     */
-    private fun division(a: Double, b: Double): Double {
-        return if (a == 0.0 || b == 0.0) {
-            0.0
-        } else {
-            abs(a) / abs(b)
+            val sum = abs(a) + abs(b)
+            val subtraction = abs(a) - abs(b)
+            val multiplication = abs(a) * abs(b)
+            val division = abs(a) / abs(b)
+            return "Сумма: $sum, разность: $subtraction, произведение: $multiplication, частность: $division"
         }
     }
+
 
     /**
      * Даны два ненулевых числа. Найти сумму, разность, произведение и
      * частное их квадратов.
      */
-    private fun sumSqrt(a: Double, b: Double): Double {
-        return if (a == 0.0 || b == 0.0) {
-            0.0
+    private fun mySqlt(a: Double, b: Double): String {
+        if (a == 0.0 || b == 0.0) {
+            error("placeholder")
         } else {
-            a.pow(2.0) + b.pow(2.0)
-        }
-    }
-
-    /**
-     * Даны два ненулевых числа. Найти сумму, разность, произведение и
-     * частное их квадратов.
-     */
-    private fun subtractionSqrt(a: Double, b: Double): Double {
-        return if (a == 0.0 || b == 0.0) {
-            0.0
-        } else {
-            a.pow(2.0) + b.pow(2.0)
-        }
-    }
-
-    /**
-     * Даны два ненулевых числа. Найти сумму, разность, произведение и
-     * частное их квадратов.
-     */
-    private fun multiplicationSqrt(a: Double, b: Double): Double {
-        return if (a == 0.0 || b == 0.0) {
-            0.0
-        } else {
-            a.pow(2.0) + b.pow(2.0)
-        }
-    }
-
-    /**
-     * Даны два ненулевых числа. Найти сумму, разность, произведение и
-     * частное их квадратов.
-     */
-    private fun divisionSqrt(a: Double, b: Double): Double {
-        return if (a == 0.0 || b == 0.0) {
-            0.0
-        } else {
-            a.pow(2.0) + b.pow(2.0)
+            val sum = a.pow(2.0) + b.pow(2.0)
+            val subtraction = a.pow(2.0) - b.pow(2.0)
+            val multiplication = a.pow(2.0) * b.pow(2.0)
+            val division = a.pow(2.0) / b.pow(2.0)
+            return "Сумма: $sum, разность: $subtraction, произведение: $multiplication, частное: $division"
         }
     }
 
     /**
      *Даны катеты прямоугольного треугольника a и b. Найти его гипотенузу c и периметр P
      */
-    private fun hypotenuse(a: Double, b: Double): Double {
-        return sqrt(a.pow(2.0) + b.pow(2.0))
-    }
-
-    /**
-     *Даны катеты прямоугольного треугольника a и b. Найти его гипотенузу c и периметр P
-     */
-    private fun perimeter(a: Double, b: Double): Double {
-        return a + b + hypotenuse(a, b)
+    private fun hypotenuse(a: Double, b: Double): Pair<Double, Double> {
+        var hypotenuse = sqrt(a.pow(2.0) + b.pow(2.0))
+        var perimeter = a + b + sqrt(a.pow(2.0) + b.pow(2.0))
+        return hypotenuse to perimeter
     }
 
     /**
@@ -205,42 +161,32 @@ class BeginKt : Command {
      * Найти площади этих кругов S1 и S2, а также площадь S3 кольца,
      * внешний радиус которого равен R1, а внутренний радиус равен R2:
      */
-    private fun squareRing(a: Double, b: Double): Double {
-        return if (square(a) - square(b) > 0) {
-            square(a) - square(b)
-        } else {
-            square(b) - square(a)
-        }
+    private fun squareRing(a: Double, b: Double): String {
+        var r1 = Math.PI * (a * a)
+        var r2 = Math.PI * (b * b)
+        return "Площадь круга  R1:" + r1 +
+                "Площадь круга  R2:" + r2 +
+                "Площадь круга R3" + (r1 - r2)
     }
 
     /**
      * Дана длина L окружности. Найти ее радиус R и площадь S круга, ог- раниченного этой окружностью,
      */
-    private fun radiusOfLength(l: Double): Double {
-        return l / PI / 2;
-    }
-
-    /**
-     * Дана длина L окружности. Найти ее радиус R и площадь S круга, ог- раниченного этой окружностью,
-     */
-    private fun squareOfLength(l: Double): Double {
-        return square(radiusOfLength(l))
+    private fun squareOfLength(l: Double): Pair<Double, Double> {
+        return (l / PI / 2) to (sqrt(l / PI / 2))
     }
 
     /**
      * Дана площадь S круга. Найти его диаметр D и длину L окружности, ограничивающей этот круг, учитывая,
      * что L = π·D, S = π·D2/4. В качестве значения π использовать 3.14.
      */
-    private fun diameterOfSquare(s: Double): Double = sqrt(s / PI / 4)
-
+    private fun diameterOfSquare(s: Double): Pair<Double, Double> {
+        return (sqrt(s / PI / 4)) to (sqrt(s / PI / 4) / PI)
+    }
 
     /**
-     * Дана площадь S круга. Найти его диаметр D и длину L окружности, ограничивающей этот круг,
-     * учитывая, что L = π·D, S = π·D2/4. В качестве значения π использовать 3.14.
+     * Найти расстояние между двумя точками с заданными координатами x1 и x2 на числовой оси: |x2 – x1|
      */
-    private fun lengthOfSquare(s: Double): Double = diameterOfSquare(s) / PI
-
-
     private fun distanceBetween(x1: Double, x2: Double): Double = x2 - x1
 
 }
